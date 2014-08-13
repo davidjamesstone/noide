83268721aab5d910bff38bf43dad9ebdbc8fda5a
David Stone <davidjamesstone@gmail.com>
2014-08-13 16:41:53 +0100

    Removed v1 files

--- noide/examples/web/hello-world/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  console.log('Hello World');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337);
-console.log('Server running at :1337/');
